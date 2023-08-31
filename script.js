c61ee3cbd10c5a6f0cdcab5034b21945fde46de9
Abraham Mejia <aimejia4@gmail.com>
2023-08-31 01:49:00 -0500

    Completed Exercise 1: Basic Function Creation

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Exercise 1: Basic Function Creation
+    Create a function named greetUser that takes a name as a parameter and returns a  greeting message.
+    The function should be able to return appropriate greetings for various names provided.
+    Commit: Once completed, commit this exercise's solution to your GitHub repository with the message: "Completed Exercise 1: Basic Function Creation".
+ */
+
+function greetUser(name){
+  return(`Hello ${name}, how are you?`);
+}
+console.log(greetUser('abraham'))
