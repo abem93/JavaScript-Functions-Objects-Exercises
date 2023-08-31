56ba68d0e47204bd40184d24168ca6c705a018df
Abraham Mejia <aimejia4@gmail.com>
2023-08-31 01:57:45 -0500

    Completed Exercise 3: Calculations Using Arrow Functions

--- script.js
+++ script.js
@@ -7,8 +7,23 @@
 function greetUser(name){
   return(`Hello ${name}, how are you?`);
 }
-console.log(greetUser('abraham'));
+console.log(greetUser('abraham')); //Hello abraham, how are you?
+
+/*
+Exercise 2: Arrow Functions
+    Convert a given traditional function into an arrow function.
+    Test the newly created arrow function with various parameters to ensure it works correctly.
+    Commit: After finishing this exercise, commit the solution with the message: "Completed Exercise 2: Arrow Functions".
+*/
 
 let userGreeting = name => (`Hello ${name}, how are you?`);
 
-console.log(userGreeting('abe'));
+console.log(userGreeting('abe'));  // Hello abe, how are you?
+
+/* Exercise 3: Calculations Using Arrow Functions
+    Create an arrow function that calculates the area of a rectangle. It should take the length and breadth as parameters.
+    Commit: Once you've achieved this, commit the solution with the message: "Completed Exercise 3: Calculations Using Arrow Functions".*/
+
+let calculateTriangleArea = (length, height) => ((length * height)/2);
+
+console.log(calculateTriangleArea(5,4)); //10
