657639808ef9e2b024cfe238e3b087c6719a6ab2
Abraham Mejia <aimejia4@gmail.com>
2023-08-31 02:08:24 -0500

    Completed Exercise 4: Object Creation and Manipulation

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ console.log(userGreeting('abe'));  // Hello abe, how are you?
 let calculateTriangleArea = (length, height) => ((length * height)/2);
 
 console.log(calculateTriangleArea(5,4)); //10
+
+/*Exercise 4: Object Creation and Manipulation
+    Construct an object named student with properties like name, age, and grade.
+    Add a method to the object that prints the student's details in the format: "NAME is AGE years old and is in GRADE grade."
+    Commit: After creating and testing this object, commit the solution with the message: "Completed Exercise 4: Object Creation and Manipulation".*/
+
+let student = {
+  name: 'timmy',
+  age: '7',
+  grade: 'second',
+  details: function(){
+    console.log(`${this.name} is ${this.age} years old and is in ${this.grade} grade.`)
+  }
+};
+
+
+student.details()
